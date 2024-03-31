5adbe011c0a3bb53d86bd3610f223d37c0cc93df
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2024-03-31 19:16:34 +0530

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 import { initFlowbite } from 'flowbite';
 import { NavComponent } from './shared/components/nav/nav.component';
 import { AuthService } from './shared/services/auth.service';
@@ -14,37 +15,33 @@ import { AuthService } from './shared/services/auth.service';
     NavComponent,
     CommonModule,
     RouterOutlet,
-    // ServiceWorkerModule.register('ngsw-worker.js', {
-    //   enabled: !isDevMode(),
-    //   registrationStrategy: 'registerWhenStable:30000'
-    // }),
   ],
 })
 export class AppComponent {
 
   constructor(
-    // private _updates: SwUpdate,
+    private _updates: SwUpdate,
     // private _snackBar: MatSnackBar
     public _authService: AuthService
   ) {
-    // _updates.versionUpdates.subscribe(evt => {
-    //   switch (evt.type) {
-    //     case 'VERSION_DETECTED':
-    //       console.info(`⌛Downloading new app version: ${evt.version.hash}`);
-    //       break;
+    _updates.versionUpdates.subscribe(evt => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.info(`⌛Downloading new app version: ${evt.version.hash}`);
+          break;
 
-    //     case 'VERSION_READY':
-    //       console.info(`👉Current app version: ${evt.currentVersion.hash}`);
-    //       console.info(`✅New app version ready for use: ${evt.latestVersion.hash}`);
-    //       const cnf = this._snackBar.open('New app version ready for use.', 'Okay');
-    //       cnf.afterDismissed().subscribe(() => location.reload());
-    //       break;
+        case 'VERSION_READY':
+          console.info(`👉Current app version: ${evt.currentVersion.hash}`);
+          console.info(`✅New app version ready for use: ${evt.latestVersion.hash}`);
+          // const cnf = this._snackBar.open('New app version ready for use.', 'Okay');
+          // cnf.afterDismissed().subscribe(() => location.reload());
+          break;
 
-    //     case 'VERSION_INSTALLATION_FAILED':
-    //       console.info(`❌Failed to install app version '${evt.version.hash}': ${evt.error}`);
-    //       break;
-    //   }
-    // });
+        case 'VERSION_INSTALLATION_FAILED':
+          console.info(`❌Failed to install app version '${evt.version.hash}': ${evt.error}`);
+          break;
+      }
+    });
   }
 
   ngOnInit(): void {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-
+// import { ServiceWorkerModule } from '@angular/service-worker';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
-        importProvidersFrom([BrowserAnimationsModule])
+        importProvidersFrom([
+            BrowserAnimationsModule,
+            // ServiceWorkerModule.register('ngsw-worker.js', {
+            //     enabled: !isDevMode(),
+            //     registrationStrategy: 'registerWhenStable:30000'
+            // }),
+        ])
     ]
 };
