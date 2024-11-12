71899a6290f2dc0f22e9df0ef5dceb1dbbe5acb4
Sujeet Jaiswara <sujeet@cleartrust.cc>
2024-11-12 19:32:10 +0530

    Fixed - Logout menu not closing

--- README.md
+++ README.md
@@ -12,3 +12,11 @@
 - Login with github
 - Metadata fetch when link paste
 - Dashboard
+
+<!-- Links -->
+
+https://flowbite.com/docs/components/toast/
+https://flowbite.com/docs/components/tooltips/
+https://flowbite.com/docs/components/progress/
+https://flowbite.com/docs/components/spinner/
+https://flowbite.com/docs/components/navbar/#typescript
--- client/src/app/shared/components/nav/nav.component.ts
+++ client/src/app/shared/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import type { DropdownInterface, DropdownOptions, InstanceOptions } from 'flowbite';
+import { Dropdown } from 'flowbite';
 import { AuthService } from '../../services/auth.service';
 import { GlobalSearchComponent } from '../global-search/global-search.component';
 
@@ -8,13 +10,34 @@ import { GlobalSearchComponent } from '../global-search/global-search.component'
   selector: 'bm-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterModule, GlobalSearchComponent],
 })
-export class NavComponent {
+export class NavComponent implements AfterViewInit {
   #router = inject(Router);
   #authService = inject(AuthService);
+  collapse!: DropdownInterface;
+
+  ngAfterViewInit(): void {
+    const $targetEl: HTMLElement | null = document.getElementById('user-dropdown');
+    const $triggerEl: HTMLElement | null = document.getElementById('user-menu-button');
+
+    const options: DropdownOptions = {
+      placement: 'bottom',
+      triggerType: 'click',
+      offsetSkidding: 0,
+      offsetDistance: 10,
+      delay: 300,
+    };
+
+    const instanceOptions: InstanceOptions = {
+      id: 'dropdownMenu',
+      override: true,
+    };
+
+    this.collapse = new Dropdown($targetEl, $triggerEl, options, instanceOptions);
+  }
 
   onAdd(e: Event) {
     e.preventDefault();
