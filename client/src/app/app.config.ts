44a0ef136f82da6b517fa2a1a02eac8c9c51485e
Sujeet Jaiswara <sujeet@cleartrust.cc>
2025-03-27 18:50:02 +0530

    Enhance bookmark management UI with improved styling and layout, including updated form elements and tag display, and integrate view transitions in router configuration.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(withFetch()),
     importProvidersFrom([
       BrowserAnimationsModule,
