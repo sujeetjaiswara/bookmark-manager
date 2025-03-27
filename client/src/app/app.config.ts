e71335b9742c896ad7ae5145e3d72be8954fff8a
Sujeet Jaiswara <sujeet@cleartrust.cc>
2025-03-27 19:05:29 +0530

    Refactor navigation component: simplify dropdown implementation and remove unused code, while updating the app configuration to disable view transitions.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes, 
+      // withViewTransitions()
+    ),
     provideHttpClient(withFetch()),
     importProvidersFrom([
       BrowserAnimationsModule,
--- client/src/app/shared/components/nav/nav.component.ts
+++ client/src/app/shared/components/nav/nav.component.ts
@@ -1,7 +1,5 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import type { DropdownInterface, DropdownOptions, InstanceOptions } from 'flowbite';
-import { Dropdown } from 'flowbite';
 import { AuthService } from '../../services/auth.service';
 import { GlobalSearchComponent } from '../global-search/global-search.component';
 
@@ -9,33 +7,12 @@ import { GlobalSearchComponent } from '../global-search/global-search.component'
   selector: 'bm-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterModule, GlobalSearchComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavComponent implements AfterViewInit {
+export class NavComponent {
   #router = inject(Router);
   #authService = inject(AuthService);
-  collapse!: DropdownInterface;
-
-  ngAfterViewInit(): void {
-    const $targetEl: HTMLElement | null = document.getElementById('user-dropdown');
-    const $triggerEl: HTMLElement | null = document.getElementById('user-menu-button');
-
-    const options: DropdownOptions = {
-      placement: 'bottom',
-      triggerType: 'click',
-      offsetSkidding: 0,
-      offsetDistance: 10,
-      delay: 300,
-    };
-
-    const instanceOptions: InstanceOptions = {
-      id: 'dropdownMenu',
-      override: true,
-    };
-
-    this.collapse = new Dropdown($targetEl, $triggerEl, options, instanceOptions);
-  }
 
   onAdd(e: Event) {
     e.preventDefault();
@@ -45,8 +22,10 @@ export class NavComponent implements AfterViewInit {
   async onLogout(e: MouseEvent) {
     e.preventDefault();
     e.stopPropagation();
+
     this.#authService.isAuthenticated.set(false);
     localStorage.removeItem('isAuthenticated');
+
     await this.#router.navigate(['login']);
   }
 }
